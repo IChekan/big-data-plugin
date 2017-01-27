2f49b987765748e154aa55109520b5bc66e889e8
Ihar_Chekan <Ihar_Chekan@epam.com>
2017-01-27 16:36:10 +0300

    Added Pentaho copyright

--- kettle-plugins/oozie/src/it/java/org/pentaho/big/data/kettle/plugins/oozie/NoArgOozieJobExecutorJobEntry.java
+++ kettle-plugins/oozie/src/it/java/org/pentaho/big/data/kettle/plugins/oozie/NoArgOozieJobExecutorJobEntry.java
@@ -1,3 +1,25 @@
+/*******************************************************************************
+ *
+ * Pentaho Big Data
+ *
+ * Copyright (C) 2002-2017 by Pentaho : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
 package org.pentaho.big.data.kettle.plugins.oozie;
 
 import org.apache.commons.vfs2.FileSystemException;
@@ -30,9 +52,6 @@ import java.util.Properties;
 
 import static org.mockito.Mockito.mock;
 
-/**
- * Created by Ihar_Chekan on 1/20/2017.
- */
 @JobEntry( id = "OozieJobExecutor", name = "Oozie.JobExecutor.PluginName",
   description = "Oozie.JobExecutor.PluginDescription",
   categoryDescription = "i18n:org.pentaho.di.job:JobCategory.Category.BigData", image = "oozie-job-executor.svg",
